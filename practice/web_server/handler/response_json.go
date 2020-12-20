c7efc30debd0412fa11f3b79ff2bd5f751bb6656
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2020-12-20 21:02:28 +0800

    2020.12.20

--- practice/web_server/handler/response_json.go
+++ practice/web_server/handler/response_json.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // Message 是一条消息应该具有的基本属性
 type Message struct {
 	Name string `json:"name"`
 	Body string `json:"body"`
-	Time int64  `json:"time"`
+	Time string `json:"time"`
 }
 
 // NewMessage 实例化 Message
@@ -19,30 +20,55 @@ func NewMessage() *Message {
 	return &Message{
 		Name: "DesistDaydream",
 		Body: "Hello World",
-		Time: 0,
+		Time: time.Now().Format("2006-01-02 15:04:05"),
 	}
 }
 
 // ResponseJSON 将会响应 JSON 格式数据
 func ResponseJSON(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("当前客户端的请求 %v 页面的 Method 为：%v\n", r.RequestURI, r.Method)
-	body, _ := ioutil.ReadAll(r.Body)
-	// r.Body.Close()
-	fmt.Printf("请求体为：%v\n", string(body))
-	// fmt.Fprint(w, string(body))
-
+	// 初始化结构体，用于存储和响应 JSON 数据
 	m := NewMessage()
-	if bs, err := json.Marshal(m); err == nil {
-		fmt.Println(string(bs))
-	} else {
-		fmt.Println(err)
-	}
+	// 声明两个常用的变量
+	var err error
+	var jsonData []byte
+	// 根据不同请求方法，执行不同的行为
+	switch r.Method {
+	case "GET":
+		// 将 struct 中的数据转换为 JSON 格式
+		if jsonData, err = json.Marshal(m); err != nil {
+			fmt.Println(err)
+			return
+		}
+		// 响应 JSON 格式的默认值
+		fmt.Fprintf(w, string(jsonData))
+	default:
+		// 模拟下面这样的 curl 请求，程序将会根据 Request Body 中的内容替换 Message 结构体数据中的值，并返回结构体中的数据
+		// 这就好比请求一个需要 TOKEN 的 API，我们只有使用正确的 TOKEN，才可以获取想要的信息
+		// curl -XPOST http://172.38.40.250:8080/json -d '{"name":"lichenhao"}'
+		//
+		// 读取 Request 的 Body
+		RequestBody, _ := ioutil.ReadAll(r.Body)
+		fmt.Printf("请求体为：%v\n", string(RequestBody))
+		// 将 Request Body 的 JSON 格式转换为 struct 类型，并将 struct 中的值替换为 JSON 中的值
+		// 注意，struct 中仅传入一个 key 的值，则 struct 中也只有一个属性的值被替代，其他属性的值保持不变
+		if err = json.Unmarshal(RequestBody, m); err != nil {
+			fmt.Fprintf(w, "请检查 Body，格式不正确或数据类型不对")
+			return
+		}
+		fmt.Printf("请求体转换为 struct 后的值为：%v\n", m)
 
-	if err := json.Unmarshal(body, &m); err == nil {
-		fmt.Println(m)
-		ret, _ := json.Marshal(m)
-		fmt.Fprint(w, string(ret))
-	} else {
-		fmt.Println(err)
+		// 根据传入的 请求体 的值，判断认证是否成功
+		// 比如现在假设，只有传入 {"name":"DesistDaydream"} 这个请求体时，才会响应结构体的数据给给客户端。
+		switch m.Name {
+		case "DesistDaydream":
+			// 认证正确，将 struct 类型数据转换为 JSON 格式数据并响应给客户端
+			if jsonData, err = json.Marshal(m); err != nil {
+				fmt.Fprintf(w, "序列化出错，请始终其他数据格式的 Body")
+			}
+			fmt.Fprint(w, string(jsonData))
+		default:
+			fmt.Fprintf(w, "你好 %v,认证失败，请重试\n", m.Name)
+		}
 	}
 }
--- web_client/web_client.go
+++ web_client/web_client.go
@@ -7,19 +7,26 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
-// User is
-type User struct {
+// Message 是一条消息应该具有的基本属性
+type Message struct {
 	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Body string `json:"body"`
+	Time string `json:"time"`
 }
 
-// Example is
-func Example() {
+// NewMessage 实例化 Message
+func NewMessage() *Message {
+	return &Message{}
+}
+
+// Client1 直接使用 http.Get() 来连接服务端
+func Client1() {
 	// net/http 标准库中还可以实现作为客户端发送 http 请求
 	// Get() 向指定的服务器发送一个 HTTP GET 请求，并返回一个 Response
-	resp, err := http.Get("http://172.38.40.250:8080")
+	resp, err := http.Get("http://172.38.40.250:8080/index")
 	if err != nil {
 		panic(err)
 	}
@@ -40,40 +47,57 @@ func Example() {
 	}
 }
 
+// Client2 先构建一个 Request，再根据这个 Request 发起请求
+func Client2() {
+	// 构建 Request
+	req, _ := http.NewRequest("GET", "http://172.38.40.250:8080/index", nil)
+	req.Header.Set("Content-type", "application/json;charset=utf-8")
+	// 根据新构建的 req 来发起请求，并获取响应信息
+	resp, _ := (&http.Client{}).Do(req)
+	// 处理响应，并输出 Response Body
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
 // GetJSON 获取 Json 数据并处理
 func GetJSON() {
-	var user User
-	user.Name = "aaa"
-	user.Age = 99
-	if bs, err := json.Marshal(user); err == nil {
-		//        fmt.Println(string(bs))
-		req := bytes.NewBuffer([]byte(bs))
-		tmp := `{"name":"junneyang", "age": 88}`
-		req = bytes.NewBuffer([]byte(tmp))
-
-		bodyType := "application/json;charset=utf-8"
-		resp, _ := http.Post("http://172.38.40.250:8080/json/", bodyType, req)
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(body))
-	} else {
+	// 第一种请求
+	// 模拟从外部读取 json 格式文件，将 json 与 struct 绑定，然后再发送请求
+	m := NewMessage()
+	// 这里假定 struct 的值时从外部文件获取的
+	m.Name = "DesistDaydream"
+	m.Body = "你好"
+	m.Time = time.Now().Format("2006-01-02 15:04:05")
+	// 将 struct 转换为 json
+	jsonData, err := json.Marshal(m)
+	if err != nil {
 		fmt.Println(err)
 	}
+	// 构建 Request
+	req, _ := http.NewRequest("POST", "http://172.38.40.250:8080/json", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-type", "application/json;charset=utf-8")
+	// 处理响应信息并输出
+	resp, _ := (&http.Client{}).Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 
-	client := &http.Client{}
-
-	// 等效于：curl
-	req := `{"name":"junneyang", "age": 88}`
-	reqNew := bytes.NewBuffer([]byte(req))
-	request, _ := http.NewRequest("POST", "http://172.38.40.250:8080/json/", reqNew)
-	request.Header.Set("Content-type", "application/json")
-	response, _ := client.Do(request)
-	if response.StatusCode == 200 {
-		body, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(body))
-	}
+	// 第二种请求
+	// 手动指定 json 数据，并发起请求
+	// 下面的代码等效于使用 crul 命令发起请求
+	// curl -XPOST http://172.38.40.250:8080/json -d '{"name":"lichenhao"}'
+	// 创建一个 json 数据
+	jsonReqBody := []byte(`{"name":"lichenhao"}`)
+	// 构建 Request
+	req, _ = http.NewRequest("POST", "http://172.38.40.250:8080/json", bytes.NewBuffer(jsonReqBody))
+	req.Header.Set("Content-type", "application/json")
+	// 处理响应信息并输出 Response Body
+	resp, _ = (&http.Client{}).Do(req)
+	body, _ = ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
 
 func main() {
-	// Example()
+	Client1()
+	Client2()
 	GetJSON()
 }
