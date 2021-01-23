a9f4832df96e75abec0e14d6904579d9e1e8cbec
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2021-01-23 20:43:49 +0800

    Update main.go

--- web_client/main.go
+++ web_client/main.go
@@ -13,8 +13,8 @@ var (
 	err  error
 )
 
-// Example1 直接使用 http.Get() 来发起请求
-func Example1() {
+// Client1 直接使用 http.Get() 来发起请求
+func Client1() {
 	// net/http 标准库中可以实现作为客户端发送 http 请求
 	// Get() 向指定的服务器发送一个 HTTP GET 请求，并返回一个 Response
 	if resp, err = http.Get("http://172.38.40.250:8080/index"); err != nil {
@@ -26,15 +26,13 @@ func Example1() {
 	// 输出服务端响应的的状态码
 	fmt.Println("Response status:", resp.Status)
 
-	// 输出 Response 中 Body 内容
-	// 处理响应，并输出 Response Body
+	// 处理 Response 中的 Body，并输出响应体的字符串格式内容
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
 }
 
-// Example2 先构建一个 Request，再根据这个 Request 发起请求
-// 除了 Example1 中的方法，还可以先构建一个 Request，再根据这个 Request 发起 http 请求，这种方式常用来自定义请求内容
-func Example2() {
+// Client2 先构建一个 Request，再根据这个 Request 发起请求，这种方式常用来自定义请求内容
+func Client2() {
 	// 构建 Request
 	req, _ = http.NewRequest("GET", "http://172.38.40.250:8080/index", nil)
 	// 为构建的 Request 设定请求头信息，可以多次使用 Set() 来设定多个 Header 信息
@@ -43,6 +41,7 @@ func Example2() {
 	fmt.Printf("本次 HTTP Request 为：%v\n请求方法为：%v\n请求头为：%v\n", req, req.Method, req.Header)
 
 	// 根据新构建的 req 来发起请求，并获取响应信息
+	// 这里的 http.Client{} 中可以设置一些发起 HTTP 请求时的一些信息，比如 TLS 等
 	if resp, err = (&http.Client{}).Do(req); err != nil {
 		panic(err)
 	}
@@ -56,8 +55,8 @@ func Example2() {
 
 func main() {
 	// 不同的方式建立与服务端的连接
-	Example1()
-	Example2()
+	Client1()
+	Client2()
 	// 像服务端传递请求体，获取 JSON 数据并处理、输出
 	GetJSON()
 }
