640ae33330dedcdc79e11fff2c9ad2c170e1a8e3
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2021-03-03 19:59:17 +0800

    Update test.go

--- web_server/handler/test.go
+++ web_server/handler/test.go
@@ -9,6 +9,7 @@ import (
 // Test is
 func Test(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("Prometheus Push 的 URL 为：%v\n", req.URL)
+	fmt.Printf("Prometheus Push 的 Method 为 %v\n", req.Method)
 	body, _ := ioutil.ReadAll(req.Body)
 	fmt.Printf("Prometheus Push 的 内容 为：%v\n", string(body))
 	fmt.Fprintf(w, "测试页面!")
