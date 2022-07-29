f8a5a3554b8034efff240194c2cc21866030d180
DesistDaydream <373406000@qq.com>
2022-07-29 16:13:59 +0800

    Update http_client.go

--- cmd/basic_http/hello_world/http_client.go
+++ cmd/basic_http/hello_world/http_client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net/http"
@@ -30,14 +31,24 @@ func Client1() {
 
 // Client2 先构建一个 Request，再根据这个 Request 发起请求，这种方式常用来自定义请求内容
 func Client2() {
-	// 构建 Request
+	// 构建 Request，主要是构建 URL、Method、Header、Body 等信息
 	req, _ = http.NewRequest("GET", "http://localhost:8080/hello", nil)
 	// 为构建的 Request 设定请求头信息，可以多次使用 Set() 来设定多个 Header 信息
 	req.Header.Set("Content-type", "application/json;charset=utf-8")
 
+	// 构建用于发起 HTTP 请求的 Client，可以指定 TLS 配置、超时时间等
+	// 这个行为其实就是实例化一个 http.Client 对象，通过这个对象可以使用 Client.Do() 方法发起 HTTP 请求
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Jar:     nil,
+		Timeout: 0,
+	}
+
 	// 根据新构建的 req 来发起请求，并获取响应信息
 	// 这里的 http.Client{} 中可以设置一些发起 HTTP 请求时的一些信息，比如 TLS 等
-	if resp, err = (&http.Client{}).Do(req); err != nil {
+	if resp, err = client.Do(req); err != nil {
 		panic(err)
 	}
 	// 关闭连接
